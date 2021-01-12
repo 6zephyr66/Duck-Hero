a25e2b234c007ce1c8d89e45e39a12a3b767263d
pixisprite <ojhaaayush123@gmail.com>
2021-01-12 21:06:38 +0530

    Got images 📷 embedded in news command 👍🏻

--- commands/news.js
+++ commands/news.js
@@ -9,13 +9,15 @@ module.exports={
         var newsList = []
         axios.get(link).then(res=>{
             const $ = cheerio.load(res.data)
-            $('.bc-title').each((index, element) => newsList[index] = {name: $(element).text().trim(), link:`https://www.cbr.com${$(element).children('a').attr('href')}`, img: $(element).parents('article').find('a > div > div > picture').first().attr('srcset').split(' ').shift()})    
+            $('.bc-title').each((index, element) => newsList[index] = {name: $(element).text().trim(), link:`https://www.cbr.com${$(element).children('a').attr('href')}`, img: $(element).parents('article').find('source').eq(0).attr('data-srcset').split(" ").shift()})
             message.reply({
                 embed:{
-                    title: newsList[args].name,
-                    color:color,
-                    description:`To read more about this topic, go to this link 👉🏻 ${newsList[args].link}`,
-                    url: newsList[args].img
+                    "title": newsList[args].name,
+                    "description":`To read more about this topic, go to this link 👉🏻 ${newsList[args].link}`,
+                    "color":color,
+                    "image":{
+                        "url":`${newsList[args].img}`
+                    }
                 }
             })
         })

7a48785187fedbb430dd32652ebbaccd0b4164da
pixisprite <ojhaaayush123@gmail.com>
2021-01-19 12:41:39 +0530

    Fixed the bugs🐛, haven't got the desired result😒

--- commands/news.js
+++ commands/news.js
@@ -1,23 +1,28 @@
+const content = require('../data/content')
+
 module.exports={
     name:'news',
     color:'skyblue',
     description:"This command will show you the news related to the anime or manga you ask for",
     async execute(message, args, color){
             let newsList = require('../main')
-            let getContent = require('../data/content')
-            message.reply({
-                embed:{
-                    "title": newsList[args].name,
-                    "description":`${getContent(newsList[args].link, 3)}`,
-                    "color":color,
-                    "image":{
-                        "url":`${newsList[args].img}`
+            const getContent = require('../data/content')
+            getContent.getContent(newsList[args].link).then(content=>{
+                message.reply({
+                    embed:{
+                        "title": newsList[args].name,
+                        "description":`${content[0]}`,
+                        "color":color,
+                        "image":{
+                            "url":`${newsList[args].img}`
+                        }
                     }
-                }
-            }).then(message => {
-                message.react('⏪')
-                message.react('⏩')    
+                }).then(message => {
+                    message.react('⏪')
+                    message.react('⏩')
+                    // console.log(getContent(newsList[args].link));
+                })
             })
-        console.log(`${getContent(newsList[args].link, 3)}`);
+            
     }
 }
--- data/content.js
+++ data/content.js
@@ -1,14 +1,14 @@
-module.exports = function getContent(link, number){
+module.exports.getContent = function getContent(link){
     const axios = require('axios')
     const cheerio = require('cheerio')
-    let result = []
+    let finalResult = []
     axios.get(link).then(res=>{
         const $ = cheerio.load(res.data)
         $('p').each((index, element)=>{
-            result[index] = $(element).text()
-            console.log($(element).text());
+            finalResult[index] = $(element).text()
         })
+        console.log(finalResult);
     })
-    console.log(result);
-    // return result[number]
+    .catch(err=> console.error(err))
+    return Promise.resolve(finalResult)
 }

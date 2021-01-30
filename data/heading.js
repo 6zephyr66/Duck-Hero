2c254d96cac0f5af778f48e7b60f7246ee206bd9
pixisprite <ojhaaayush123@gmail.com>
2021-01-30 13:04:27 +0530

    few tweaks 😅

--- data/heading.js
+++ data/heading.js
@@ -5,7 +5,7 @@ module.exports  = function getHeading(){
     let newsList = []
     axios.get(link).then(res=> {
         const $ = cheerio.load(res.data)
-        $('.bc-title').each((index, element) => newsList[index] = {name: $(element).text().trim(), link:`https://www.cbr.com${$(element).children('a').attr('href')}`, img: $(element).parents('article').find('source').eq(0).attr('data-srcset').split(" ").shift()})
+        $('.bc-title').each((index, element) => newsList[index] = {name: $(element).text().trim(), link:`https://www.cbr.com${$(element).children('a').attr('href')}`, image: $(element).parents('article').find('source').eq(0).attr('data-srcset').split(" ").shift()})
     })
     return newsList
 } 

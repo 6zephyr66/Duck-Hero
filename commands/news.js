748ab74fa2ffd0928d40962bc3a176888ad999c2
pixisprite <ojhaaayush123@gmail.com>
2021-02-09 17:56:35 +0530

    The news command now works properly ☺
    Now, we can change the paragraphs of the news on our command 😌

--- commands/news.js
+++ commands/news.js
@@ -1,29 +1,73 @@
-const content = require('../data/content')
-
 module.exports={
     name:'news',
     color:'skyblue',
     description:"This command will show you the news related to the anime or manga you ask for",
     async execute(message, args, color){
             let newsList = require('../main')
+            let newArg = args - 1
+            let newsNumber = 0
+            if(newArg<0) {
+                message.reply("Enter a number greater than 0 😡😠")
+                return
+            }
+            else{ 
             const getContent = require('../data/content')
-            getContent(newsList[args].link).then(content=>{
-                console.log("Message sent")
+            getContent(newsList[newArg].link).then(content=>{
                 message.reply({
                     embed:{
-                        "title": newsList[args].name,
-                        "description":`${content[1]}`,
+                        "title": newsList[newArg].name,
                         "color":color,
                         "image":{
-                            "url":`${newsList[args].img}`
-                        }
+                            "url":`${newsList[newArg].image}`
+                        },
+                        "description":`${content[newsNumber]}`
                     }
                 }).then(message => {
-                    message.react('⏪')
-                    message.react('⏩')
-                    // console.log(getContent(newsList[args].link));
+
+                    const collector = message.createReactionCollector((reaction, user)=>{
+                        return reaction.emoji.name === '⏩' || reaction.emoji.name === '⏪';
+                    },{time:300000})
+
+                    Promise.all([
+                        message.react('⏪'),
+                        message.react('⏩')
+                    ])
+                    collector.on('collect', (reaction, user)=>{
+                        console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
+                        if(reaction.emoji.name === "⏩"){
+                            newsNumber+=1;
+                            message.edit({
+                                embed:{
+                                    "title":newsList[newArg].name,
+                                    "color":color,
+                                    "image":{
+                                        "url":`${newsList[newArg].image}`
+                                    },
+                                    "description":`${content[newsNumber]}`
+                                }
+                            })
+                        }
+                        else if(reaction.emoji.name === "⏪"){
+                            newsNumber-=1;
+                            message.edit({
+                                embed:{
+                                    "title":newsList[newArg].name,
+                                    "color":color,
+                                    "image":{
+                                        "url":`${newsList[newArg].image}`
+                                    },
+                                    "description":`${content[newsNumber]}`
+                                }
+                            })
+                        }
+                    })
+
+                    collector.on('end', collected=>{
+                        console.log(`Collected ${collected.size} items`);
+                    })
                 })
             })
             .catch(err=>console.error(err))
+        }
     }
 }
